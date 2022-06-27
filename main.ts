745581a9424252f4eee465ba097b02a421688f1a
Kururi2Pi <83536496+Kururi2Pi@users.noreply.github.com>
2022-06-27 17:33:16 +0900

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Yes)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.No)
+})
+bluetooth.startAccelerometerService()
+bluetooth.startButtonService()
+bluetooth.startLEDService()
+basic.showIcon(IconNames.Square)
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.happy), SoundExpressionPlayMode.UntilDone)
+    } else if (input.buttonIsPressed(Button.B)) {
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
+    }
 })
