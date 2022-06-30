a1648cb24966b72cb5a7a4e94422c23c1c7091a0
Kururi2Pi <83536496+Kururi2Pi@users.noreply.github.com>
2022-06-30 10:37:02 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,14 +4,36 @@ bluetooth.onBluetoothConnected(function () {
 bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.No)
 })
-bluetooth.startAccelerometerService()
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.UntilDone)
+})
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.happy), SoundExpressionPlayMode.UntilDone)
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
+    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_UP) {
+        basic.showIcon(IconNames.EigthNote)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_2_UP) {
+        basic.showIcon(IconNames.EigthNote)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_3_UP) {
+        basic.showIcon(IconNames.EigthNote)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_4_UP) {
+        basic.showIcon(IconNames.EigthNote)
+    }
+    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_DOWN) {
+        count += 1
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_2_DOWN) {
+        count += -1
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_3_DOWN) {
+        count += -10
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_4_DOWN) {
+        count += 10
+    }
+})
+let count = 0
 bluetooth.startButtonService()
 bluetooth.startLEDService()
 basic.showIcon(IconNames.Square)
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        music.playSoundEffect(music.builtinSoundEffect(soundExpression.happy), SoundExpressionPlayMode.UntilDone)
-    } else if (input.buttonIsPressed(Button.B)) {
-        music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
-    }
+    basic.showNumber(count)
 })
